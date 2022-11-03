8603d4d439ee53233ddcdf8bc625b2079ff7ddcb
Pedro <pedro-edu19@outlook.com>
2022-11-02 23:38:15 -0300

    resolvido bug teste de arquivos antes de upload

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\BaseController;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use App\Traits\UploadAble;
 use App\Models\Setting;
 
@@ -27,7 +28,7 @@ class SettingController extends BaseController
      */
     public function update(Request $request)
     {   
-        if ($request->hasFile('site_logo') && $request->file('site_logo')->isValid()) {
+        if ($request->has('site_logo') && ($request->file('site_logo') instanceof UploadedFile)) {
 
             if (config('settings.site_logo') != null) {
                 $this->deleteOne(config('settings.site_logo'));
@@ -35,7 +36,7 @@ class SettingController extends BaseController
             $logo = $this->uploadOne($request->file('site_logo'), 'img');
             Setting::set('site_logo', $logo);
 
-        } elseif ($request->has('site_favicon') && $request->file('site_favicon')->isValid()) {
+        } elseif ($request->has('site_favicon') && ($request->file('site_favicon') instanceof UploadedFile)) {
 
             if (config('settings.site_favicon') != null) {
                 $this->deleteOne(config('settings.site_favicon'));
