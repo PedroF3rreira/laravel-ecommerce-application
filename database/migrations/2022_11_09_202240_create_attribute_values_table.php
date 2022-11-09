75257bb888ddaedf086c837a20beb01612a1432a
Pedro Daniel <pedro-edu19@outlook.com>
2022-11-09 17:37:11 -0300

    adicionando modelo e migrations attributes e Attributes value

---
+++ database/migrations/2022_11_09_202240_create_attribute_values_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Attribute;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Attribute::class)->references('id')->on('attributes');
+            $table->text('value');
+            $table->decimal('price', 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attributes', function (Blueprint $table) {
+            $table->dropForeignIdFor(Attribute::class);
+        });
+        Schema::dropIfExists('attribute_values');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminsTableSeed::class);
         $this->call(SettingsTableSeed::class);
         $this->call(CategoriesTableSeed::class);
+        $this->call(AttributesTableSeed::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
